akraino-edge-stack/ta-infra-ansible

commit b2e13865c2b899ce48148879da20323bd9bc988c

disk wipeoff: Fix shell syntax for missing disk
When one of the disks defined in the vendors/product configuration
files is not present on the system, the wipeoff commands fail due to
likely broken shell syntax and duplicate commands.

Signed-off-by: Alexandru Antone <Alexandru.Antone@enea.com>
Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
Change-Id: I667d85bb0b3e47ae5297d8eb3b16df84bdc7317a

infra-ansible.spec
@@ -15,7 +15,7 @@
 
 Name:           infra-ansible
 Version:        %{_version}
-Release:        4%{?dist}
+Release:        5%{?dist}
 Summary:        Contains ansible playbook and roles for Akraino rec blueprint
 License:        %{_platform_licence}
 Source0:        %{name}-%{version}.tar.gz

playbooks/destroy_data_and_partitions.yml
@@ -14,20 +14,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+- name: check whether disks exist and are block devices
+  stat: path={{ item }}
+  with_items: "{{ disks }}"
+  register: disks_stat
+
 - name: wipe off filesystems from disks
-  shell: "[ -b {{ item }} ] | /usr/sbin/wipefs --all --force {{ item }} || /usr/sbin/wipefs --all --force {{ item }}"
+  command: "/usr/sbin/wipefs --all --force {{ item.item }}"
   args:
     creates: /etc/storage/osd_disk_metadata.json
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk
 
 - name: destroy data from disks
-  shell: "[ -b {{ item }} ] | dd if=/dev/zero of={{ item }} count=200 bs=1M"
+  command: "dd if=/dev/zero of={{ item.item }} count=200 bs=1M"
   args:
     creates: /etc/storage/osd_disk_metadata.json
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk
 
 - name: destroy partitions from disks
-  shell: "[ -b {{ item }} ] | /usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item }} || /usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item }}"
+  command: "/usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item.item }}"
   args:
     creates: /etc/storage/osd_disk_metadata.json
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk

roles/cleanup_disks/tasks/destroy_data_and_partitions.yml
@@ -14,20 +14,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+- name: check whether disks exist and are block devices
+  stat: path={{ item }}
+  with_items: "{{ disks }}"
+  register: disks_stat
+
 - name: wipe off filesystems from disks
-  shell: "[ -b {{ item }} ] | /usr/sbin/wipefs --all --force {{ item }} || /usr/sbin/wipefs --all --force {{ item }}"
+  command: "/usr/sbin/wipefs --all --force {{ item.item }}"
   args:
     creates: "{{ creates_file }}"
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk
 
 - name: destroy data from disks
-  shell: "[ -b {{ item }} ] | dd if=/dev/zero of={{ item }} count=200 bs=1M"
+  command: "dd if=/dev/zero of={{ item.item }} count=200 bs=1M"
   args:
     creates: "{{ creates_file }}"
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk
 
 - name: destroy partitions from disks
-  shell: "[ -b {{ item }} ] | /usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item }} || /usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item }}"
+  command: "/usr/sbin/sgdisk --zap-all --clear --mbrtogpt -- {{ item.item }}"
   args:
     creates: "{{ creates_file }}"
-  with_items: "{{ disks }}"
+  with_items: "{{ disks_stat.results }}"
+  when: item.stat.exists and item.stat.isblk

commit 7385fe7b00e999a7c89edf0523708563c7947b17

Fix ansible disk wipeoff skip
Previous commit b2e1386 switched from using a shell `-b` test to
using the ansible `stat` module for block device checks.
However, Ansible `stat` does not follow symlinks by default,
leading to some block devices being skipped, which left old
metadata in place, triggering an OSD activation failure as reported
in [1].

[1]
https://github.com/ceph/ceph-ansible/issues/2945#issuecomment-411720133

Fixes: b2e13865c2b899ce48148879da20323bd9bc988c

Reported-by: Krisztian Lengyel <krisztian.lengyel@nokia.com>
Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
Change-Id: Ie0f00cea2f9ff70a123b007a54e18122846456f3

infra-ansible.spec
@@ -15,7 +15,7 @@
 
 Name:           infra-ansible
 Version:        %{_version}
-Release:        7%{?dist}
+Release:        8%{?dist}
 Summary:        Contains ansible playbook and roles for Akraino rec blueprint
 License:        %{_platform_licence}
 Source0:        %{name}-%{version}.tar.gz

playbooks/destroy_data_and_partitions.yml
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 - name: check whether disks exist and are block devices
-  stat: path={{ item }}
+  stat: path={{ item }} follow=true
   with_items: "{{ disks }}"
   register: disks_stat
 

roles/cleanup_disks/tasks/destroy_data_and_partitions.yml
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 - name: check whether disks exist and are block devices
-  stat: path={{ item }}
+  stat: path={{ item }} follow=true
   with_items: "{{ disks }}"
   register: disks_stat
 